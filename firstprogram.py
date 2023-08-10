cba6f073548acf58058616a7c0442d6c384f017a
modit1802 <122519231+modit1802@users.noreply.github.com>
2023-08-10 13:43:43 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,10 @@
+lis=[]
+n=int(input("Enter the elements of list:"))
+for i in range(n):
+    x=int(input())
+    lis.append(x)
+
+print("---------------------------- List printing ----------------------------")
+for i in lis:
+    print(i)
+
